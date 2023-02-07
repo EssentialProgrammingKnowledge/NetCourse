944d394aaf40445114ea95351b2a8b6a4f38049a
KW <kamil.wojtasinski.poczta@gmail.com>
2023-02-07 23:34:12 +0100

    Added projects from Module 6

---
+++ IQyerableVsIEnumerable/Actor.cs
@@ -0,0 +1,8 @@
+﻿namespace IQyerableVsIEnumerable
+{
+    public class Actor
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = $"Name{Guid.NewGuid():N}";
+    }
+}
---
+++ IQyerableVsIEnumerable/DbContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace IQyerableVsIEnumerable
+{
+    internal class TestDbContext : DbContext
+    {
+        private readonly ILoggerFactory? _loggerFactory;
+
+        public DbSet<Actor> Actors { get; set; }
+
+        public TestDbContext()
+        {
+        }
+
+        public TestDbContext(ILoggerFactory loggerFactory)
+        {
+            _loggerFactory = loggerFactory;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("DataSource=:memory:");
+            if (_loggerFactory is not null)
+            {
+                optionsBuilder.UseLoggerFactory(_loggerFactory);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
